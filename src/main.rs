d08b19332c1190774637f912dcf0bf7639b18889
= <un.known.river.person@gmail.com>
2025-02-13 17:25:18 +0300

    Added works_on

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use os_info::get as os_get;
 use std::fs;
 use toml;
 
@@ -26,6 +27,17 @@ fn main() {
             return;
         }
     };
+    let os_name = os_get().os_type().to_string().to_lowercase();
 
-    println!("{:?}", parsed.sys.works_on);
+    if !(parsed.sys.works_on.get(0) == Some(&"all".to_string()))
+        && !parsed
+            .sys
+            .works_on
+            .iter()
+            .any(|x| x.to_lowercase() == os_name)
+    {
+        println!("This script does not work in your system. If you wrote an uncorrect name check https://crates.io/crates/os_info.");
+    } else {
+        println!("This script is supported on your system");
+    }
 }
